f5c230d7993f32f29ac789964d52f205346c2be3
MaSir <793405823@qq.com>
2022-02-07 10:25:54 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,24 +1,32 @@
 <?php
- function encrypt(){
-    $userid = "123456";
-    $serialize  = false; $wrap  = true;
-    $iv = random_bytes(16);
-    $data = 'This is a test'; // 加密字符串
-    $encrypted  = Pkcs::getInstance()->encrypt($iv, $data,$serialize, $wrap);
-    if ($encrypted['code'] == 200) {
-        print_r($encrypted);exit();
-    }
- }
 
-  function decrypt($iv,$encrypted){
+class Test {
+  public function encrypt(){
+     $userid = "123456";
+     $serialize  = false; $wrap  = true;
+     $iv = random_bytes(16);
+     $data = 'This is a test'; // 加密字符串
+     $encrypted  = Pkcs::getInstance()->encrypt($iv, $data,$serialize, $wrap);
+     if ($encrypted['code'] == 200) {
+         // 完成后把iv的值和$encrypted['data']的值储存，解密的过程中使用
+         print_r($encrypted);exit();
+     }
+  }
+
+  public function decrypt($iv,$encrypted){
       $userid = "123456";
       $serialize  = false; $wrap  = true;
       $decrypted  = Pkcs::getInstance()->decrypt($iv, $encrypted, $serialize, $wrap);
       print_r($decrypted);
   }
 
-encrypt();
+}
 
-// iv和encrypted是已经储存好的值
-decrypt($iv,$encrypted);
+$test = new Test ();
+
+// 加密
+$test->encrypt();
+
+//解密 iv和encrypted是已经储存好的值
+$test->decrypt($iv,$encrypted);
 
